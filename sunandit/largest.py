2892593975aab2668b56bba5fe4b4344a14f42e1
sayan801 <sayan801@gmail.com>
2016-09-09 17:25:45 +0530

    programs done sunandit on 8th sep

---
+++ sunandit/largest.py
@@ -0,0 +1,11 @@
+num1= input( " enter  the  first  number:" )
+num2=input("enter  the   second  number: " )
+num3=input(" enter  the  3rd  number: ")
+if (num1>num2) and (num1>num3):
+print(" largest  number  is  num1")
+elif (num2>num1)and (num2>num3)
+print("num2")
+elif(num3>num1)and (num3>num2)
+print("num3")
+			
+
