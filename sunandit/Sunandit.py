025e76e02c87613b4105199695a62e7a4abb426b
sunandit-technicise <sunandit.chaudhuri@gmail.com>
2016-09-15 13:50:06 +0530

    Add files via upload

---
+++ sunandit/Sunandit.py
@@ -0,0 +1,7 @@
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+temp = a
+a = b
+b = temp
+print("the value of a after swapping: {}" .format(a))
+print("the value of b after swapping: {}" .format(b))
